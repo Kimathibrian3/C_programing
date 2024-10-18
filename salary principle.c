ea51cbb9a86e9c2eaf86503c339986d9e8c7f957
Kimathibrian3 <kimathibrian023@gmail.com>
2024-10-18 12:08:06 +0300

    Add files via upload

---
+++ salary principle.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main(){
+//declaration and initialization
+char a = 'K';
+char name[]={"John"};
+int age =18;
+float salary=2000;   //%f
+double area =20; //%if
+
+printf("The character is \t %c \n",a);
+printf("The name(string) is %s \n",name);
+printf("The age (18) is %d \n",age);
+printf("The salary(float) is % 2f \n",salary);
+
+
+ return 0;
+}
---
+++ simple_interest1 (1).c
@@ -0,0 +1,30 @@
+/*
+Author Brian kimathi 
+Reg CT102/G/23094/24
+Date 2023/9/26
+*/
+// processer directive_scanf
+
+#include<stdio.h>
+
+int main(){
+
+float principle,Time,Rate,simple_interest;
+// Accepte user input
+printf("enter principle amount,\n");
+scanf("%f",&principle);
+
+printf("enter time(in years):,\n");
+scanf("%f",&Time);
+
+printf("enter Rate in interest:,\n");
+scanf("%f",&Rate);
+
+//calculate simple interest 
+simple_interest=(principle*Rate*Time)/100;
+
+//display results 
+printf("The simple interest is:%.2f\n", simple_interest);
+
+ return 0;
+}
